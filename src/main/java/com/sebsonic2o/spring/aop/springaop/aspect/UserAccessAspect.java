ef0dbce70c5d3523286872e59734127676791826
Sebastien Cheraux <scheraux@gmail.com>
2020-12-30 14:18:15 -0800

    define sample user access before aspect for cross cutting security concern

---
+++ src/main/java/com/sebsonic2o/spring/aop/springaop/aspect/UserAccessAspect.java
@@ -0,0 +1,23 @@
+package com.sebsonic2o.spring.aop.springaop.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+@Aspect
+@Configuration
+public class UserAccessAspect {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	// intercept method calls before execution by defining "pointcut"
+	@Before("execution(* com.sebsonic2o.spring.aop.springaop.business.*.*(..))")
+	public void doBeforeAction(JoinPoint joinPoint) {
+		// intercept logic aka "advice"
+		logger.info("Check for user access");
+		logger.info("Allow access for {}", joinPoint);
+	}
+}
