6ddd38a5ddc72eef4e66c5db89f94bf84d3465a1
Sebastien Cheraux <scheraux@gmail.com>
2020-12-30 13:06:17 -0800

    initialize maven project with spring aop

---
+++ src/test/java/com/sebsonic2o/spring/aop/springaop/SpringAopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sebsonic2o.spring.aop.springaop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
